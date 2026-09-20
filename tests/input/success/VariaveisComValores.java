cf7af7e311b59725da1e7666ca963c497f3d6945
LuisGFNunes <ferreiranunesluisgustavo447@gmail.com>
2026-09-20 17:48:25 -0300

    fix(test): corrige diff em relatorio de testes

---
+++ tests/input/success/VariaveisComValores.java
@@ -0,0 +1,11 @@
+class VariaveisComValores {
+    public static void main(String[] args) {
+        int i = 0;
+        long l = 0;
+
+        float f = 0.0;
+        double d = 0.0;
+
+        char c = 'c';
+    }
+}
